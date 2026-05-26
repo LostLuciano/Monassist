2d377972f257ffa57a64dfc064482d9372ab738c
LostLuciano <lucianolost@github.com>
2026-05-27 00:52:07 +0700

    chore: prepare backend for Koyeb and Neon Tech deployment

--- moneyassist-backend/config/database.php
+++ moneyassist-backend/config/database.php
@@ -5,6 +5,7 @@ return [
     'connections' => [
         'pgsql' => [
             'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', 5432),
             'database' => env('DB_DATABASE', 'moneyassist'),
@@ -14,7 +15,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
     ],
     'migrations' => 'migrations',
